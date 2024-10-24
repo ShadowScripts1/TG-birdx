124b3930061cde94576db322cda2c271830cef40
odesigo <chidera@winexviv.com>
2024-10-24 16:44:21 +0100

    happy hunting

---
+++ config/banner.js
@@ -0,0 +1,7 @@
+function printBanner() {
+  console.log("========== BirdSUI BOT ==========");
+  console.log("GitHub: https://github.com/ShadowScripts1");
+  console.log("Telegram: https://web.telegram.org/k/#-2448067261");
+  console.log("=================================");
+}
+module.exports = printBanner;
